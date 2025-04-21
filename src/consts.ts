91a0bf4c923668b68c0c51b09ca5ac9770d03133
Nathaniel Tampus <nate@mugnavo.com>
2025-04-21 16:16:04 +0800

    update intro text

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "nize";
 export const SITE_DESCRIPTION =
-  "A software engineer from the Philippines who loves to build stuff on the web.";
+  "A software engineer and self-proclaimed nerd who loves to build stuff on the web.";
 
 export interface LayoutProps {
   title?: string;
