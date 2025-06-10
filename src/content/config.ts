3e71b1ff9934bf766369f6e25baea98e34fc2816
Nathaniel Tampus <nate@mugnavo.com>
2025-06-11 03:21:51 +0800

    update latest post

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/posts" }),
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/posts" }),
 
   schema: z.object({
     title: z.string(),
