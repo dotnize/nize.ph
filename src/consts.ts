a6ffd07088abc831c46934fc2fa20835ce8bb35e
Nathaniel John Tampus <nat@nize.ph>
2026-02-10 13:50:29 +0800

    update intro

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "nize";
 export const SITE_DESCRIPTION =
-  "Multi-disciplinary software engineer from the Philippines focused on building web products.";
+  "Multi-disciplinary software engineer from the Philippines who loves building stuff on the web.";
 
 export interface LayoutProps {
   title?: string;
